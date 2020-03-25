741cf7116d4de25f0e8ca2da4f55c02df1122345
秦继贤 <1653764529@qq.com>
2020-03-25 15:36:05 +0800

    LanguageRanking

--- index.js
+++ index.js
@@ -1,47 +1,112 @@
-$(function () {
-    var myChart = echarts.init(document.getElementById('ranking'));
+var dataObject = [
+    {
+        name: "Java",
+        rank: 1,
+        change: '降',
+        onedChng: -0.0001
+    },
+    {
+        name: "C",
+        rank: 2,
+        change: '升',
+        onedChng: +0.0244
+    },
+    {
+        name: "Python",
+        rank: 3,
+        change: '升',
+        onedChng: +0.0141
+    },
+    {
+        name: "C++",
+        rank: 4,
+        change: '降',
+        onedChng: -0.0258
+    },
+    {
+        name: "C#",
+        rank: 5,
+        change: '升',
+        onedChng: +0.0207
+    },
+    {
+        name: "Visual Basic .NET",
+        rank: 6,
+        change: '降',
+        onedChng: -0.0117
+    },
+    {
+        name: "JavaScript",
+        rank: 7,
+        change: '降',
+        onedChng: -0.0085
+    }
+]
 
-    var option = {
-        title: {
-            text: 'JavaScript语言排名变化'
-        },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data: ['排名']
-        },
-        xAxis: {
-            data: ['2000', '2005', '2010', '2015', '2020']
-        },
-        yAxis: {},
-        series: [{
-            name: '排名',
-            type: 'line',
-            data: [6, 9, 8, 8, 7],
-        }]
-    };
-    myChart.setOption(option);
-});
 
-var data = [
-    ['java', 1, '降', '-0.01%'],
-    ['C', 2, '升', '+2.44%'],
-    ['Python', 3, '升', '+1.41%'],
-    ['C++', 4, '降', '-2.58%'],
-    ['C#', 5, '升', '+2.07%'],
-    ['Visual Basic .NET', 6, '降', '-1.17%'],
-    ['JavaScript', 7, '降', '-0.85%']
-];
+var hotElement = document.querySelector('#rank');
+var hotElementContainer = hotElement.parentNode;
+var hotSettings = {
+    data: dataObject,
+    columns: [
+        {
+            data: 'name',
+            type: 'text',
+        },
+        {
+            data: 'rank',
+            type: 'numeric',
+        },
+        {
+            data: 'change',
+            type: 'text'
+        },
+        {
+            data: 'onedChng',
+            type: 'numeric',
+            numericFormat: {
+                pattern: '0.00%'
+            }
+        }
+    ],
+    stretchH: 'all',
+    width: 600,
+    maxRows: 20,
+    manualRowMove: true,
+    manualColumnMove: true,
+    colHeaders: [
+        '语言名称',
+        '排名',
+        '升或降',
+        '变化幅度'
+    ],
+    contextMenu: true,
+    filters: true,
+    manualRowResize: true,
+    manualColumnResize: true
+};
+var hot = new Handsontable(hotElement, hotSettings);
 
-var container = document.getElementById('rank');
 
-var hot = new Handsontable(container, {
-    data: data,
-    rowHeaders: false,
-    colHeaders: true,
-    colHeaders: ['语言名称', '排名', '升或降', '变化幅度'],
-    rowHeights: 40,
-    colWidths: 200,
-    className: 'htCenter htMiddle'
-});
+var myChart = echarts.init(document.getElementById('jsrank'));
+
+var option = {
+    title: {
+        text: 'JavaScript语言排名变化'
+    },
+    tooltip: {},
+    legend: {
+        data: ['排名']
+    },
+    xAxis: {
+        data: ["2000", "2005", "2010", "2015", "2020"]
+    },
+    yAxis: {},
+    series: [{
+        name: '排名',
+        type: 'line',
+        data: [6, 9, 8, 8, 7]
+    }]
+};
+
+myChart.setOption(option);
